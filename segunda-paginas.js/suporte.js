28669ffbb3d679e4a69f22fb839567cae70b0c55
renegador42-lab <renegador42@gmail.com>
2026-02-14 21:05:47 -0300

    Atualização do projeto

--- segunda-paginas.js/suporte.js
+++ segunda-paginas.js/suporte.js
@@ -1,8 +1,19 @@
-  const btnMenu = document.querySelector("#btnMenu");
-  const menu = document.querySelector("#menu");
+const form = document.getElementById("formSuporte");
+const mensagemSucesso = document.getElementById("mensagemSucesso");
 
-  btnMenu.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-  });
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
 
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const assunto = document.getElementById("assunto").value;
+  const mensagem = document.getElementById("mensagem").value;
 
+  if (nome === "" || email === "" || assunto === "" || mensagem === "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+
+  mensagemSucesso.textContent = "Solicitação enviada com sucesso!";
+  form.reset();
+});
